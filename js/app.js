bd5d287c88bf1b0390a972254072812e7fdd8e37
Jacob Koch <jacobtkoch@gmail.com>
2014-12-03 23:35:40 -0800

    Made the map grab traffic cameras and filter by search.

--- js/app.js
+++ js/app.js
@@ -11,3 +11,56 @@
 //you should also write the code to filter the set of markers when the user
 //types a search phrase into the search box
 
+
+$(document).ready(function () {
+    var mapElem = document.getElementById('map');
+    var center = {
+        lat: 47.6,
+        lng: -122.3
+    };
+    var map = new google.maps.Map(mapElem, {
+        center: center,
+        zoom: 12
+    });
+
+    var infoWindow = new google.maps.InfoWindow();
+
+    $.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
+        .done(function (data) {
+            data.forEach(function (camera) {
+                var marker = new google.maps.Marker({
+                    position: {
+                        lat: Number(camera.location.latitude),
+                        lng: Number(camera.location.longitude)
+                    },
+                    map: map
+                });
+
+                google.maps.event.addListener(marker, 'click', function() {
+                    map.panTo(marker.getPosition());
+                    var html = '<p>' + camera.cameralabel + '</p>';
+                    html += '<img src="' + camera.imageurl.url + '"></img>';
+
+                    infoWindow.setContent(html);
+                    infoWindow.open(map, this);
+                });
+
+                $('#search').bind('search keyup', function() {
+                    var search = $('#search').val().toLowerCase();
+
+                    if (camera.cameralabel.toLowerCase().indexOf(search) < 0) {
+                        marker.setMap(null);
+                    }
+                    else {
+                        marker.setMap(map);
+                    }
+                });
+            });
+        })
+        .fail(function (error) {
+            alert("Error: " + error);
+        })
+        .always(function () {
+
+        });
+});
